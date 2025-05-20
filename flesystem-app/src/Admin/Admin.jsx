76ba85765d1a6288aebe3c8a9a3d7e1f1ea0e50c
Juan Villegas <juandvp0303@gmail.com>
2025-05-19 23:44:11 -0400

    :

--- flesystem-app/src/Admin/Admin.jsx
+++ flesystem-app/src/Admin/Admin.jsx
@@ -262,7 +262,7 @@ const Admin = () => {
             {console.log(selectedProduct)}
           <Typography variant="h6" >Precio unitario para {selectedProduct?.product_name}</Typography>
           <Typography variant="body2" sx={{color:"gray"}}>Actual precio de venta: {selectedProduct?.sell_price} </Typography>
-          <Typography variant="body2" sx={{color:"gray"}}>Último precio de compra: {selectedProduct?.last_completed_order_price_unit} </Typography>
+          <Typography variant="body2" sx={{color:"gray"}}>Último precio de compra: {(selectedProduct?.last_completed_order_price_unit) || 0} </Typography>
           <TextField
             label="Precio unitario" 
             type="number" 
